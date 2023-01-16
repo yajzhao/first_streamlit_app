6d7484ad2a675ec1118f1d253cfde8913e9d98b6
Yajing Zhao <33792643+yajzhao@users.noreply.github.com>
2023-01-16 10:54:44 -0800

    Update streamlit_app.py
    using requests python lib for get http

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,3 +20,9 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 # display the table 
 streamlit.dataframe(fruits_to_show)
+
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+
+streamlit.header("Fruityvice Fruit Advice!")
+streamlit.text(fruityvice_response.json())
